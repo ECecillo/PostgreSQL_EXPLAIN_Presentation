4048ec028a062de7e644e39565d1fa51f837ed3d
ECecillo <enzocecillon@MacBook-Pro-de-Enzo.local>
2024-04-10 17:55:53 +0200

    chore: configure deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://postgresql-slides.ececillo.fr",
-	base: "/slides",
+	site: "https://ececillo.github.io",
+	base: "/PostgreSQL_EXPLAIN_Presentation",
 });
